bfb8fcf8923bffe39f681dc38274669c8888d48d
Lucas Ferreira <ferreira.lkz@gmail.com>
2017-12-14 17:51:26 -0200

    finalizando aula 5

--- classificacao.py
+++ classificacao.py
@@ -12,12 +12,24 @@ marcacoes = [1, 1, 1, -1, -1 , -1]
 
 misterioso1 = [1, 1, 1]
 misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
-teste = [misterioso1, misterioso2]
+testes = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, -1]
 
 from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
 
-print(modelo.predict(teste))
+resultado = modelo.predict(testes)
+print "\nresultado"
+print(resultado)
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d==0] 
+print "\nacertos"
+print(acertos)
+
+print "\nTaxa de acerto"
+print(100.0*len(acertos)/len(testes)) 
